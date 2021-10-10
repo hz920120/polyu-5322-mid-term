f50c473f81074907d07784c953acf614ced6a532
huangzuo <huangzuo@mi-ya.com.cn>
2021-10-10 20:48:04 +0800

    fix

---
+++ js/js.js
@@ -0,0 +1,11 @@
+(function($) {
+    /*------------------
+    TYPED JS
+--------------------*/
+    $(".element").typed({
+        strings: ["I'm Michael Owen", "Web Designer", "From Sydney"],
+        typeSpeed: 10,
+        loop:true,
+        backDelay: 2000
+    });
+})(jQuery);
