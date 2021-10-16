51da14dcfa5a0fa72efa728016f6078c01e51a49
huangzuo <huangzuo@mi-ya.com.cn>
2021-10-16 16:42:09 +0800

    version 1.0 commit

---
+++ js/contact.js
@@ -0,0 +1,29 @@
+function jump_fb(){
+    var winHandler = window.open('', '_blank');
+    winHandler.location.href = 'https://www.facebook.com/profile.php?id=100009640625392';
+}
+
+function jump_twi(){
+    var winHandler = window.open('', '_blank');
+    winHandler.location.href = 'https://twitter.com/huangzuocesc';
+}
+
+function jump_ins(){
+    var winHandler = window.open('', '_blank');
+    winHandler.location.href = 'https://www.instagram.com/huangzuocesc/';
+}
+
+function jump_git(){
+    var winHandler = window.open('', '_blank');
+    winHandler.location.href = 'https://github.com/hz920120';
+}
+
+function jump_weibo(){
+    var winHandler = window.open('', '_blank');
+    winHandler.location.href = 'https://weibo.com/1172613352/profile?topnav=1&wvr=6';
+}
+
+function jump_wechat(){
+    var winHandler = window.open('', '_blank');
+    winHandler.location.href = '../images/wechat.png';
+}
